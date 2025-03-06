1215c95e2c47a0a922360719457ed7771eac820a
2210030388 <2210030388@klh.edu.in>
2025-03-06 14:12:30 +0530

    Initial commit

---
+++ js/globalScope.js
@@ -0,0 +1,12 @@
+var Apple="red";
+let Orange="yellow";
+const banana = "green";
+
+function fruit()
+{
+    document.write(Apple);
+    document.write(Orange);
+    document.write(banana);
+
+}
+fruit();
