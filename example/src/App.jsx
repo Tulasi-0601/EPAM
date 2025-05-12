f2694d59f23e4b1358e9e79231dc6a84982345f2
Tulasi-0601 <tulasinalluri165@gmail.com>
2025-05-12 17:06:44 +0530

    Initial commit

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -28,27 +28,29 @@ function App() {
   const [hoveredIndex, setHoveredIndex] = useState(null);
 
   return (
-    
     <div className="container">
-      
-      {friends.map((friend, index) => (
-        <div
-          className="image-wrapper"
-          key={index}
-          onMouseEnter={() => setHoveredIndex(index)}
-          onMouseLeave={() => setHoveredIndex(null)}
-        >
-          <img src={friend.image} alt={friend.name} className="friend-image" />
-          {hoveredIndex === index && (
-            <div className="info-popup">
-              <h3>{friend.name}</h3>
-              <p>{friend.info}</p>
-            </div>
-          )}
-        </div>
-      ))}
+      <h1 className="title">Project Mates</h1> {/* ✅ Title comes first */}
+      <div className="friends-row">
+        {friends.map((friend, index) => (
+          <div
+            className="image-wrapper"
+            key={index}
+            onMouseEnter={() => setHoveredIndex(index)}
+            onMouseLeave={() => setHoveredIndex(null)}
+          >
+            <img src={friend.image} alt={friend.name} className="friend-image" />
+            {hoveredIndex === index && (
+              <div className="info-popup">
+                <h3>{friend.name}</h3>
+                <p>{friend.info}</p>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
+
 
 export default App;
